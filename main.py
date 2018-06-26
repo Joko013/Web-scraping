ebb4f8251cd9abf1d98faf227fe202d4e762e9c1
Joko013 <30841710+Joko013@users.noreply.github.com>
2018-06-26 19:46:06 +0200

    V 2.1.1
    add encoding

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# This Python file uses the following encoding: utf-8
+
 import web_scraper
 import web_page
 
--- web_scraper.py
+++ web_scraper.py
@@ -1,3 +1,5 @@
+# This Python file uses the following encoding: utf-8
+
 import csv
 import requests
 from bs4 import BeautifulSoup
